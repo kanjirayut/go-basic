51fea971ac66df6b4ac854eaa0e919fb9b3ca78b
jirayut <>
2025-01-06 21:38:26 +0700

    swagger

--- cmd/api/docs/docs.go
+++ cmd/api/docs/docs.go
@@ -522,7 +522,9 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:7070",
+	// Host:             "localhost:8080",local
+	// Host:             "localhost:7070",docker local
+    Host:             "go-basic-yyh0.onrender.com",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "Movies API with GO and PostgreSQL",
